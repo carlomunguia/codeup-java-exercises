b56da483d49445f3050e9c9ec16460cdc0012ab4
Carlo Munguia <carlomunguia@carlos-air.home>
2019-03-21 11:46:31 -0500

    finished exercise 2

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,11 +1,14 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
-        while (i <= 15) {
-            System.out.println("i is totally " + i);
+        int i = 0;
+        do {
+            System.out.println("i is " + i * 2);
             i++;
-        }
+        } while (i < 100);
     }
 }
+
+
+
 
 
