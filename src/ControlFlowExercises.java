0d8d7c5075a304d6a1361f74a1cb2c920d59e405
Carlo Munguia <carlomunguia@carlos-air.home>
2019-03-22 10:33:20 -0500

    working out and finishing java string exercises

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,13 +1,29 @@
-public class ControlFlowExercises {
-    public static void main(String[] args) {
-        int i = 0;
-        do {
-            System.out.println("i is " + i * 2);
-            i++;
-        } while (i < 100);
-    }
-}
-
+//import java.util.Scanner;
+//
+//public class ControlFlowExercises {
+//    public static void main(String[] args) {
+//        String answer;
+//        do {
+//            System.out.println("Please enter an integer: ");
+//
+//            Scanner banana = new Scanner(System.in);
+//
+//            int num = banana.nextInt();
+//
+//            System.out.printf("number | squared | cubed%n");
+//            System.out.printf("------ | ------ | -------%n");
+//
+//            for (int i = 1; i <= num; i++) {
+//                System.out.printf("%-6s | %-6d | %d%n", i, i * i, i * i * i);
+//            }
+//
+//            System.out.println();
+//            System.out.println("Would you like to continue? (y/n)");
+//        }while(i <= 1000);
+//    }
+//}
+//
+//
 
 
 
