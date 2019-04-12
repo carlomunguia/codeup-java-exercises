e01ffc55add1cee24a7e0091ffcc0370f0bf1f85
Carlo Munguia <carlomunguia@carlos-air.home>
2019-04-12 15:37:26 -0500

    making sure all work is complete before push

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -1,78 +1,78 @@
-//package grades;
-//
-//import util.Input;
-//
-//import java.util.*;
-//
-//public class GradesApplication {
-//    static Map<String,Student> students;
-//
-//    public static void init() {
-//        students = new HashMap<>();
-//        Student dwight = new Student("Dwight");
-//        dwight.addGrade(90);
-//        dwight.addGrade(85);
-//        dwight.addGrade(400);
-//
-//        Student bethany = new Student("Bethany");
-//        bethany.addGrade(95);
-//        bethany.addGrade(86);
-//        bethany.addGrade(100);
-//
-//        Student marcus = new Student("Marcus");
-//        marcus.addGrade(117);
-//        marcus.addGrade(250);
-//        marcus.addGrade(30);
-//
-//        Student adrien = new Student("Adrien");
-//        adrien.addGrade(43);
-//        adrien.addGrade(26);
-//        adrien.addGrade(0);
-//        adrien.addGrade(147000);
-//
-//        students.put("dbmister", dwight);
-//        students.put("bethanyramirez", bethany);
-//        students.put("marcus100", marcus);
-//        students.put("adrienVL", adrien);
-//
-//    }
-//    public static void welcome() {
-//        System.out.printf("Welcome!\n");
-//        String usernames = "|";
-//        for (String key : students.keySet())
-//            usernames += " " + key + " |";
-//        System.out.printf("\n" +
-//                "Here are the github usernames of our students:\n" +
-//                "\n" +
-//                usernames);
-//    }
-//
-//    public static Map<String, Student> getStudents() {
-//        return students;
-//    }
-//
-//    public static void setStudents(Map<String, Student> students) {
-//        GradesApplication.students = students;
-//    }
-//
-//    public static void main(String[] args) {
-//        Input input;
-//        input = new Input();
-//        init();
-//        welcome();
-//        boolean keepGoing = true;
-//
-//        do {
-//            System.out.println("What student are you trying to find?");
-//            String studentChoice = input.getString();
-//            System.out.println(studentChoice);
-//
-//
-//
-//
-//        }while
-//
-//            @
-//
-//    }
-//}
+package grades;
+
+import util.Input;
+
+import java.util.*;
+
+public class GradesApplication {
+    static Map<String,Student> students;
+
+    public static void init() {
+        students = new HashMap<>();
+        Student dwight = new Student("Dwight");
+        dwight.addGrade(90);
+        dwight.addGrade(85);
+        dwight.addGrade(400);
+
+        Student bethany = new Student("Bethany");
+        bethany.addGrade(95);
+        bethany.addGrade(86);
+        bethany.addGrade(100);
+
+        Student marcus = new Student("Marcus");
+        marcus.addGrade(117);
+        marcus.addGrade(250);
+        marcus.addGrade(30);
+
+        Student adrien = new Student("Adrien");
+        adrien.addGrade(43);
+        adrien.addGrade(26);
+        adrien.addGrade(0);
+        adrien.addGrade(147000);
+
+        students.put("dbmister", dwight);
+        students.put("bethanyramirez", bethany);
+        students.put("marcus100", marcus);
+        students.put("adrienVL", adrien);
+
+    }
+    public static void welcome() {
+        System.out.printf("Welcome!\n");
+        String usernames = "|";
+        for (String key : students.keySet())
+            usernames += " " + key + " |";
+        System.out.printf("\n" +
+                "Here are the github usernames of our students:\n" +
+                "\n" +
+                usernames);
+    }
+
+    public static Map<String, Student> getStudents() {
+        return students;
+    }
+
+    public static void setStudents(Map<String, Student> students) {
+        GradesApplication.students = students;
+    }
+
+    public static void main(String[] args) {
+        Input input;
+        input = new Input();
+        init();
+        welcome();
+        boolean keepGoing = true;
+
+        do {
+            System.out.println("What student are you trying to find?");
+            String studentChoice = input.getString();
+            System.out.println(studentChoice);
+
+
+
+
+        }while
+
+            @
+
+    }
+}
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,69 +1,35 @@
-//package shapes;
-//
-//import java.awt.geom.AffineTransform;
-//import java.awt.geom.PathIterator;
-//import java.awt.geom.Point2D;
-//import java.awt.geom.Rectangle2D;
-//
-//public class Square extends Rectangle{
-//    @Override
-//    public void setLength(double length) {
-//
-//    }
-//
-//    @Override
-//    public java.awt.Rectangle getBounds() {
-//        return null;
-//    }
-//
-//    @Override
-//    public Rectangle2D getBounds2D() {
-//        return null;
-//    }
-//
-//    @Override
-//    public boolean contains(double x, double y) {
-//        return false;
-//    }
-//
-//    @Override
-//    public boolean contains(Point2D p) {
-//        return false;
-//    }
-//
-//    @Override
-//    public boolean intersects(double x, double y, double w, double h) {
-//        return false;
-//    }
-//
-//    @Override
-//    public boolean intersects(Rectangle2D r) {
-//        return false;
-//    }
-//
-//    @Override
-//    public boolean contains(double x, double y, double w, double h) {
-//        return false;
-//    }
-//
-//    @Override
-//    public boolean contains(Rectangle2D r) {
-//        return false;
-//    }
-//
-//    @Override
-//    public PathIterator getPathIterator(AffineTransform at) {
-//        return null;
-//    }
-//
-//    @Override
-//    public PathIterator getPathIterator(AffineTransform at, double flatness) {
-//        return null;
-//    }
-//
-//    protected double side;
+
+package shapes;
+
+public class Square extends Quadrilateral {
+
+    public Square(double side) {
+        super(side, side);
+    }
+
+    @Override
+    public void setLength(double length) {
+        this.length = length;
+        this.width = length;
+    }
+
+    @Override
+    public void setWidth(double width) {
+        this.length = width;
+        this.width = width;
+    }
+    ////    protected double side;
 //
 //    public Square(double side) {
-//        super(side, side);
+////        this.side = side;
+//        super(side,side);
 //    }
-//}
+//
+    public double getArea() {
+        return this.length * this.length;
+    }
+
+    public double getPerimeter() {
+        return 4 * this.length;
+    }
+}
